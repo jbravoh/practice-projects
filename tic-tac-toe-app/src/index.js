f4436e6dad4d29bd93db4654019be95a8e07c2f4
jbravoh <jbravoharriott@gmail.com>
2020-12-13 22:29:41 +0000

    finalised code

--- tic-tac-toe-app/src/index.js
+++ tic-tac-toe-app/src/index.js
@@ -115,12 +115,14 @@ class Game extends React.Component {
       );
     });
     
+   
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+   
 
     return (
       <div className="game">
@@ -130,7 +132,7 @@ class Game extends React.Component {
           onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div className="status">{status}</div>
           <ol>{moves}</ol>
         </div>
       </div>
