7bc0a1fb80f10f3b78cd50acf9af88fd673b1deb
jbravoh <jbravoharriott@gmail.com>
2020-12-13 21:41:41 +0000

    Moved board components to game components

--- tic-tac-toe-app/src/index.js
+++ tic-tac-toe-app/src/index.js
@@ -17,50 +17,19 @@ function Square (props) {
 // Keeping the state of all squares in the Board component will allow it to determine the winner in the future.
 // the Square components are now controlled components. The Board has full control over them.
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      //Each time a player moves xIsNext will be flipped to determine which player goes next
-      xIsNext: true, 
-    }
-  }
-
-  handleClick(i) {
-    // we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
-    const squares = this.state.squares.slice(); 
-      //ignores click if someone has won
-      if (calculateWinner(squares) || squares[i]) {
-        return;
-      }
-    //“X”s and “O”s can take turns
-    squares[i] = this.state.xIsNext ? 'X' : 'O'; 
-    this.setState({
-      squares:squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   renderSquare(i) {
     return (
     //passes down function from the Board to the Square - square will call function when a square is clicked
     <Square 
       value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      // board component now receives squares and onClick props from the Game component
+      onClick={() => this.props.onClick(i)}
       />
     );
   }
  
+  //Refactored the Board's render function as the game component is now rending the game's status
   render() {
-    //declares a winner
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -85,20 +54,73 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  // create initial state for the game component within its constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    // we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
+    const squares = this.state.squares.slice(); 
+      //ignores click if someone has won
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+    //“X”s and “O”s can take turns
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  
+  // updated the Game component’s render function to use the most recent history entry to determine and display the game’s status
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+          squares={current.squares}
+          onClick={(i) => this.handleClick(i)}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
 }
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
 
 function calculateWinner(squares) {
   const lines = [
@@ -120,35 +142,4 @@ function calculateWinner(squares) {
   return null;
 }
 
-history = [
-  //Before first move
-  {
-    squares: [
-      null, null, null,
-      null, null, null,
-      null, null, null,
-    ]
-  },
-  //After first move
-  {
-    squares: [
-      null, null, null,
-      null, 'X', null,
-      null, null, null,
-    ]
-  },
-]
-
-// ========================================
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
